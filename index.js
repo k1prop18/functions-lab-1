db10fabb03c43d7896dfdc33cd988cd8fdf5fc68
Caleb Kiprop Rono <ckiprop14@gmail.com>
2023-06-18 17:31:18 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
 
 // Code your solution in this file!
 function distanceFromHqInBlocks(value) {
-    if (`${value}` <= 42) {
-      return 42 - `${value}`;
-    } else {
-      return `${value}` - 42;
-    }
+  if (value <= 42) {
+    return 42 - value;
+  } else {
+    return value - 42;
   }
-  function distanceFromHqInFeet(feet) {
-    if (`${feet}` <= 42) {
-      return (42 - `${feet}`) * 264;
-    } else {
-      return (`${feet}` - 42) * 264;
-    }
+}
+function distanceFromHqInFeet(value) {
+  if (value <= 42) {
+    return (42 - value) * 264;
+  } else {
+    return (value - 42) * 264;
   }
-  
-  function distanceTravelledInFeet(start, destination) {
-    if (destination < start) {
-      return (start - destination) * 264;
-    } else {
-      return (`${destination} ` - `${start}`) * 264;
-    }
+}
+
+function distanceTravelledInFeet(a, b) {
+  if (a <= b) {
+    return (b - a) * 264;
+  } else {
+    return (a - b) * 264;
   }
-  function calculatesFarePrice(start, destination) {
-    if ((start - destination) * 264 <= 400) {
-      return 0;
-    } else if (
-      (start - destination) * 264 >= 400 &&
-      (start - destination) * 264 <= 2000
-    ) {
-      return 2.56;
-    } else if ((destination - start) * 264 > 2000) {
-      return 25;
-    } else if ((start - destination) * 264 > 2500) {
-      return "cannot travel that far";
-    }
+}
+
+let a = "start"
+let b = "destination"
+
+function calculatesFarePrice(a, b) {
+  if (distanceTravelledInFeet(a, b) < 400) {
+    return 0;
+  } 
+  else if (distanceTravelledInFeet(a, b) > 400 && distanceTravelledInFeet(a, b) <= 2000) {
+    return (distanceTravelledInFeet(a, b) - 400) * 0.02
   }
+  else if (distanceTravelledInFeet(a, b) > 2000 && distanceTravelledInFeet(a, b) < 2500) {
+    return 25;
+  } 
+  else if (distanceTravelledInFeet(a, b) > 2500) {
+    return 'cannot travel that far';
+  }
+}
+
